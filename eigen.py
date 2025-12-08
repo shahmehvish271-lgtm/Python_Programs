896ad00939c7c5b3ab8ee58733e3ea097fb6b631
shahmehvish271-lgtm <shahmehvish271@gmail.com>
2025-12-08 15:12:53 +0530

    Add files via upload

---
+++ eigen.py
@@ -0,0 +1,12 @@
+# class employee:
+#     def __init__(self,name,age,salary):
+#         self.name=name
+#         self.age=age
+#         self.salary=salary
+#     def yearly_salary(self):
+#         yearly=self.salary*12
+#         print(f"yearly salary of {self.name} is:{yearly}")
+
+# emp1 = employee("Burhan",18,1000)
+# emp1.yearly_salary()
+
