18e797e28afbc04b748e2bdc723ecf821988692c
shahmehvish271-lgtm <shahmehvish271@gmail.com>
2025-12-14 11:36:51 +0530

    Python1.py

---
+++ 4-Dec-2025/Python1.py
@@ -0,0 +1,6 @@
+# create and write to a file
+file = open("file.txt", "w")
+file.write("Welcome to Python File Handling\n")
+file.write("This file is created using Python\n")
+file.close()
+print("File created using Python")
