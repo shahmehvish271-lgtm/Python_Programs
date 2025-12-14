6640544c4cfb91dd2b8042c710022998d8cf65ac
shahmehvish271-lgtm <shahmehvish271@gmail.com>
2025-12-14 11:28:31 +0530

    Python_3.py

---
+++ 20-Nov-25/Python_3.py
@@ -0,0 +1,10 @@
+l = [6, 8, 9, 1, 2, 19, 18]
+
+minimum = min(l)
+maximum = max(l)
+summ = sum(l)
+
+print("List = ", l)
+print("Minimum = ", minimum)
+print("Maximum = ", maximum)
+print("Sum = ", summ)
